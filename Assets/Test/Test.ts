8eb4d87fe9fd31ec323c872fc0634eb4d6e4b155
Dong-yeob <rkato0128@naver.com>
2022-06-29 01:54:05 +0900

    리소스 삭제 전 기록

---
+++ Assets/Test/Test.ts
@@ -0,0 +1,8 @@
+import { TextMeshPro, TextMeshProUGUI } from 'TMPro'
+import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+
+export default class Test extends ZepetoScriptBehaviour {
+
+    public text : TextMeshProUGUI;
+
+}
