75b813bf89f4f7ba05d2125dbcae6b77ac6830d1
Dong-yeob <rkato0128@naver.com>
2022-06-29 22:40:48 +0900

    프로젝트 재생성

--- Assets/Test/Test.ts
+++
@@ -1,8 +0,0 @@
-import { TextMeshPro, TextMeshProUGUI } from 'TMPro'
-import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
-
-export default class Test extends ZepetoScriptBehaviour {
-
-    public text : TextMeshProUGUI;
-
-}
