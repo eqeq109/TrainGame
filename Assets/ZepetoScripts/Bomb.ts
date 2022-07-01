9384955dab3004f9626159c0b78a1dec94735479
Dong-yeob <rkato0128@naver.com>
2022-07-01 22:51:27 +0900

    오브젝트 사운드 적용 / SoundManager 프리팹화 및 SoundManager 스크립트 추가

--- Assets/ZepetoScripts/Bomb.ts
+++ Assets/ZepetoScripts/Bomb.ts
@@ -1,88 +1,103 @@
 import { ZepetoScriptBehaviour } from "ZEPETO.Script";
 import * as UnityEngine from "UnityEngine";
 import {
-  GameObject,
-  Vector3 as UnityVector3,
-  Object,
-  Transform,
-  Time,
-  Mathf,
-  Quaternion,
-  BoxCollider,
-  Rigidbody,
-  FixedJoint,
-  Projector,
+    GameObject,
+    Vector3 as UnityVector3,
+    Object,
+    Transform,
+    Time,
+    Mathf,
+    Quaternion,
+    BoxCollider,
+    Rigidbody,
+    FixedJoint,
+    Projector,
 } from "UnityEngine";
 import { Collider } from "UnityEngine";
 import { easing } from "./EasingFunctions";
 import { ZepetoCharacter } from "ZEPETO.Character.Controller";
 
 export default class Bomb extends ZepetoScriptBehaviour {
-  public triggerEvent: Function;
-  public returnEvent: Function;
-  public indicatorFill: Projector;
-  public objectBomb: Transform;
-  public particleExplode: UnityEngine.ParticleSystem;
+    public triggerEvent: Function;
+    public returnEvent: Function;
+    public indicatorFill: Projector;
+    public objectBomb: Transform;
+    public particleExplode: UnityEngine.ParticleSystem;
 
-  private animationTime: number = 2;
-  private animation: UnityEngine.Coroutine;
-  private detectionAvailable: boolean = false;
+    //SFX
+    public sfxExplosion:GameObject;
 
-  public set setTrigger(value: Function) {
-    this.triggerEvent = value;
-  }
+    private animationTime: number = 2;
+    private animation: UnityEngine.Coroutine;
+    private detectionAvailable: boolean = false;
 
-  public Init(event: Function, returnObject: Function, position: UnityVector3) {
-    this.triggerEvent = event;
-    this.returnEvent = returnObject;
-    if (this.animation != null) this.StopCoroutine(this.SetAnimation);
-    this.objectBomb.transform.localPosition = new UnityEngine.Vector3(0, 20, 0);
-    this.gameObject.transform.position = position;
-    this.objectBomb.gameObject.SetActive(true);
-    this.animation = this.StartCoroutine(this.SetAnimation());
-  }
-
-  private *SetAnimation() {
-    let timer: float = 0;
-    const indicatorMax: number = 2.5;
-    const startPosition: UnityEngine.Vector3 = this.objectBomb.localPosition;
-
-    while (timer < this.animationTime) {
-      timer += Time.deltaTime;
-      let timeRatio: float = timer / this.animationTime;
-      this.objectBomb.localPosition = new UnityEngine.Vector3(
-        easing.inOutBounce(startPosition.x, 0, timeRatio),
-        easing.inOutBounce(startPosition.y, 0, timeRatio),
-        easing.inOutBounce(startPosition.z, 0, timeRatio)
-      );
-      // this.objectBomb.localPosition = UnityEngine.Vector3.Lerp(startPosition,
-      //     UnityEngine.Vector3.zero, timeRatio);
-      this.indicatorFill.orthographicSize = indicatorMax * timeRatio;
-      yield null;
+    public set setTrigger(value: Function) {
+        this.triggerEvent = value;
     }
-    this.objectBomb.gameObject.SetActive(false);
-    this.particleExplode.gameObject.SetActive(true);
-    this.particleExplode.Play();
-    timer = 0;
-    this.detectionAvailable = true;
-    yield new UnityEngine.WaitForSeconds(0.3);
-    this.detectionAvailable = false;
-    yield new UnityEngine.WaitForSeconds(0.7);
-    this.particleExplode.gameObject.SetActive(false);
-    this.returnEvent(this.gameObject);
-    yield null;
-  }
 
-  Start() {}
+    public Init(event: Function, returnObject: Function, position: UnityVector3) {
+        this.triggerEvent = event;
+        this.returnEvent = returnObject;
+        if (this.animation != null) this.StopCoroutine(this.SetAnimation);
+        this.objectBomb.transform.localPosition = new UnityEngine.Vector3(0, 20, 0);
+        this.gameObject.transform.position = position;
+        this.objectBomb.gameObject.SetActive(true);
+        this.animation = this.StartCoroutine(this.SetAnimation());
+    }
 
-  OnTriggerStay(coll: Collider) {
-    if (!this.detectionAvailable) return;
-    if (!coll.gameObject.GetComponent<ZepetoCharacter>()) {
-      return;
+    private *SetAnimation() {
+        let timer: float = 0;
+        const indicatorMax: number = 2.5;
+        const startPosition: UnityEngine.Vector3 = this.objectBomb.localPosition;
+
+        //for SFX check
+        let sfxCheck:bool = false;
+
+        while (timer < this.animationTime) {
+            timer += Time.deltaTime;
+            let timeRatio: float = timer / this.animationTime;
+            this.objectBomb.localPosition = new UnityEngine.Vector3(
+                easing.inOutBounce(startPosition.x, 0, timeRatio),
+                easing.inOutBounce(startPosition.y, 0, timeRatio),
+                easing.inOutBounce(startPosition.z, 0, timeRatio)
+            );
+            // this.objectBomb.localPosition = UnityEngine.Vector3.Lerp(startPosition,
+            //     UnityEngine.Vector3.zero, timeRatio);
+            
+            //Play Exlosion SFX
+            if(timer >= 1.75 && !sfxCheck)
+            {
+                this.sfxExplosion.SetActive(true);
+                sfxCheck = true;
+            }
+
+            this.indicatorFill.orthographicSize = indicatorMax * timeRatio;
+            yield null;
+        }
+        this.objectBomb.gameObject.SetActive(false);
+        this.particleExplode.gameObject.SetActive(true);
+        this.particleExplode.Play();
+        timer = 0;
+        this.detectionAvailable = true;
+        yield new UnityEngine.WaitForSeconds(0.3);
+        this.detectionAvailable = false;
+        yield new UnityEngine.WaitForSeconds(0.7);
+        this.particleExplode.gameObject.SetActive(false);
+        this.sfxExplosion.SetActive(false);
+        this.returnEvent(this.gameObject);
+        yield null;
     }
-    if (this.triggerEvent != null) {
-      this.detectionAvailable = false;
-      this.triggerEvent(coll.gameObject.GetInstanceID(), this.gameObject);
+
+    Start() { }
+
+    OnTriggerStay(coll: Collider) {
+        if (!this.detectionAvailable) return;
+        if (!coll.gameObject.GetComponent<ZepetoCharacter>()) {
+            return;
+        }
+        if (this.triggerEvent != null) {
+            this.detectionAvailable = false;
+            this.triggerEvent(coll.gameObject.GetInstanceID(), this.gameObject);
+        }
     }
-  }
 }
---
+++ Assets/ZepetoScripts/SoundManager.ts
@@ -0,0 +1,33 @@
+import { AudioClip, AudioSource } from 'UnityEngine'
+import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+
+export default class SoundManager extends ZepetoScriptBehaviour {
+
+    public soundPlayer:AudioSource;
+    
+    public sfxLevelUp:AudioClip;
+    public sfxLevelDown:AudioClip;
+    public sfxGetPoint:AudioClip;
+    public sfxGameOver:AudioClip;
+
+    public PlaySound(action:string)
+    {
+        switch(action)
+        {
+            case "LevelUp":
+                this.soundPlayer.clip = this.sfxLevelUp;
+                break;
+            case "LevelDown":
+                this.soundPlayer.clip = this.sfxLevelDown;
+                break;
+            case "GetPoint":
+                this.soundPlayer.clip = this.sfxGameOver;
+                break;
+            case "GameOver":
+                this.soundPlayer.clip = this.sfxGameOver;
+                break;
+        }
+
+        this.soundPlayer.Play();
+    }
+}
