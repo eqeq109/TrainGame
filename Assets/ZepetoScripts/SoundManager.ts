38ad6157926c8f1141a3c8c4cf82a955da73b1ea
eqeq1090 <eqeq1090@gmail.com>
2022-07-01 23:12:40 +0900

    사운드 매니저 이동

--- Assets/ZepetoScripts/SoundManager.ts
+++ Assets/ZepetoScripts/SoundManager.ts
@@ -1,33 +1,30 @@
-import { AudioClip, AudioSource } from 'UnityEngine'
-import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+import { AudioClip, AudioSource } from "UnityEngine";
+import { ZepetoScriptBehaviour } from "ZEPETO.Script";
 
 export default class SoundManager extends ZepetoScriptBehaviour {
+  public soundPlayer: AudioSource;
 
-    public soundPlayer:AudioSource;
-    
-    public sfxLevelUp:AudioClip;
-    public sfxLevelDown:AudioClip;
-    public sfxGetPoint:AudioClip;
-    public sfxGameOver:AudioClip;
+  public sfxLevelUp: AudioClip;
+  public sfxLevelDown: AudioClip;
+  public sfxGetPoint: AudioClip;
+  public sfxGameOver: AudioClip;
 
-    public PlaySound(action:string)
-    {
-        switch(action)
-        {
-            case "LevelUp":
-                this.soundPlayer.clip = this.sfxLevelUp;
-                break;
-            case "LevelDown":
-                this.soundPlayer.clip = this.sfxLevelDown;
-                break;
-            case "GetPoint":
-                this.soundPlayer.clip = this.sfxGameOver;
-                break;
-            case "GameOver":
-                this.soundPlayer.clip = this.sfxGameOver;
-                break;
-        }
-
-        this.soundPlayer.Play();
+  public PlaySound(action: string) {
+    switch (action) {
+      case "LevelUp":
+        this.soundPlayer.clip = this.sfxLevelUp;
+        break;
+      case "LevelDown":
+        this.soundPlayer.clip = this.sfxLevelDown;
+        break;
+      case "GetPoint":
+        this.soundPlayer.clip = this.sfxGetPoint;
+        break;
+      case "GameOver":
+        this.soundPlayer.clip = this.sfxGameOver;
+        break;
     }
+
+    this.soundPlayer.Play();
+  }
 }
